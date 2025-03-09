3624231faf3570c38338b053c3c3bec6e0e30aea
Tri Susilo <trisusilo.developer@gmail.com>
2025-03-09 20:48:38 +0700

    change the database connection methode

--- config/database.go
+++ config/database.go
@@ -3,6 +3,7 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,9 +11,30 @@ import (
 var MasterDB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "stnduser:stnduser@tcp(192.168.1.145:6033)/go_crud?parseTime=true")
+	// Read database credentials from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Check if any variable is missing
+	if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" {
+		log.Fatal("Database environment variables are not set")
+	}
+
+	// Build DSN (Data Source Name)
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?parseTime=true"
+
+	// Open database connection
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Test the connection
+	if err := db.Ping(); err != nil {
+		log.Fatal("Database connection test failed:", err)
 	}
 
 	log.Println("Connected to database Master")
